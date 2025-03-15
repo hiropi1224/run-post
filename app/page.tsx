03f1439ca5e0f9e47083f61e21235363ddc8c99f
shirahama <64931333+hiropi1224@users.noreply.github.com>
2025-03-15 19:01:26 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p>hello world</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    dynamicIO: true,
+    ppr: true,
+  },
+};
+
+export default nextConfig;
