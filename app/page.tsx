2aabcf92fa2fe6e34c5d0b60dae9108af18f23b9
hiropi1224 <64931333+hiropi1224@users.noreply.github.com>
2025-03-16 22:26:52 +0900

    ✨ trpc

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import { ClientGreeting } from "~/app/components/client-greeting";
 import { Button } from "~/components/ui";
+import { HydrateClient, trpc } from "~/trpc/server";
 
 export default function Home() {
+  void trpc.hello.prefetch({
+    text: "client",
+  });
+
   return (
-    <div className="">
-      <p>hello world</p>
-      <Button>Click me</Button>
-    </div>
+    <HydrateClient>
+      <div className="">
+        <p>hello world</p>
+        <Button>Click me</Button>
+        <ClientGreeting />
+      </div>
+    </HydrateClient>
   );
 }
