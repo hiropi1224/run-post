4c8ade1bb3420c01c9bb80e12497acd046bd3911
hiropi1224 <64931333+hiropi1224@users.noreply.github.com>
2025-04-06 23:39:15 +0900

    👔

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,19 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { jsonb, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const posts = pgTable("posts", {
   id: uuid("id").primaryKey(),
   content: text("content").notNull(),
+  templateId: uuid("template_id").references(() => templates.id),
+  activityType: text("activity_type").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const templates = pgTable("templates", {
+  id: uuid("id").primaryKey(),
+  name: text("name").notNull(),
+  content: text("content").notNull(),
+  type: text("type").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
